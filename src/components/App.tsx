6a44e3268e410f555307f77823e03b8ba3411cc3
manjaka13 <manjaka.rajaonson@gmail.com>
2023-10-02 12:33:35 +0200

    dislays the right screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,29 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
+import EmptyLayout from "src/components/Layout/EmptyLayout";
+import SignIn from "src/components/Page/SingIn";
+import { UserContext } from "src/hooks/useUser";
+import Loading from "src/components/Page/Loading";
 
 /**
  * React entry point
  */
 
 const App: FC = (): JSX.Element => {
-	return <h1>Salama app</h1>;
+	const { loading, user } = useContext(UserContext);
+
+	console.log(user);
+
+	return loading ? (
+		<Loading />
+	) : user ? (
+		<EmptyLayout>
+			<p>Dashboard</p>
+		</EmptyLayout>
+	) : (
+		<EmptyLayout>
+			<SignIn />
+		</EmptyLayout>
+	);
 };
 
 export default App;
