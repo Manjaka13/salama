db409876ab392f1b4099cc5a5bb713681435f362
manjaka13 <manjaka.rajaonson@gmail.com>
2023-10-02 13:30:24 +0200

    add icons

--- src/helpers/icons.ts
+++ src/helpers/icons.ts
@@ -1,8 +1,12 @@
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
-import { faHeart, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+	faHeart,
+	faSearch,
+	faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 /**
  * Export icons from here
  */
 
-export { Icon, faHeart, faSearch };
+export { Icon, faHeart, faSearch, faCaretDown };
