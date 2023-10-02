68be8d5f6dca5d4793033fd8d39ff834cbeaaf87
manjaka13 <manjaka.rajaonson@gmail.com>
2023-10-02 13:45:11 +0200

    add conversation header

--- src/components/Dashboard/Conversation.tsx
+++ src/components/Dashboard/Conversation.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react";
+import { AVATAR_DEFAULT } from "src/helpers/const";
 
 /**
  * This component displays the current conversation
  */
 
 const Conversation: FC = (): JSX.Element => {
+	const k: number = 2;
+
 	return (
 		<div className="conversation">
+			<div className="conversation__head">
+				<img className="avatar" src={AVATAR_DEFAULT} alt="Avatar" />
+				<p>Tantely</p>
+				<div className={`status${k % 2 === 0 && " status--active"}`}></div>
+			</div>
 			<p>Conversation</p>
 		</div>
 	);
