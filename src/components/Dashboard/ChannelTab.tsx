a643355db78f883d16c17981dd867025462112b4
manjaka13 <manjaka.rajaonson@gmail.com>
2023-10-02 13:33:08 +0200

    add channels tab

--- src/components/Dashboard/ChannelTab.tsx
+++ src/components/Dashboard/ChannelTab.tsx
@@ -1,13 +1,56 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
+import { Icon, faCaretDown } from "src/helpers/icons";
+import { UserContext } from "src/hooks/useUser";
+import { AVATAR_DEFAULT } from "src/helpers/const";
 
 /**
  * Lists channels here
  */
 
 const ChannelTab: FC = (): JSX.Element => {
+	const { user } = useContext(UserContext);
+	const channelList: string[] = ["General", "Jokes", "Novity"];
+	const dmList: string[] = [
+		user?.displayName?.split(" ")[0] || "You",
+		"Evah",
+		"Tantely",
+		"Maharavo",
+	];
+
+	// List of channels to be displayed
+	const mappedChannels: JSX.Element[] = channelList.map(
+		(item: string, key: number) => (
+			<li className="channel-tab__item" key={key}>
+				<div className="content"># {item}</div>
+			</li>
+		)
+	);
+
+	// List of direct messages to be displayed
+	const mappedDms: JSX.Element[] = dmList.map((item: string, key: number) => (
+		<li className="channel-tab__item" key={key}>
+			<div className="content">
+				<img className="avatar" src={AVATAR_DEFAULT} alt="Avatar" /> {item}
+			</div>
+			<div className={`status${key % 2 === 0 && " status--active"}`}></div>
+		</li>
+	));
+
 	return (
-		<div className="channel-tab">
-			<p>Channels</p>
+		<div className="channel-tab no-select">
+			{/* Channels */}
+			<h2 className="channel-tab__title">
+				<Icon className="icon" icon={faCaretDown} /> Channels
+			</h2>
+			<ul className="channel-tab__list">{mappedChannels}</ul>
+
+			<div className="channel-tab__divider"></div>
+
+			{/* DMs */}
+			<h2 className="channel-tab__title">
+				<Icon className="icon" icon={faCaretDown} /> Direct messages
+			</h2>
+			<ul className="channel-tab__list">{mappedDms}</ul>
 		</div>
 	);
 };
